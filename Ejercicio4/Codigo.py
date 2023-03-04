3eadf673081692c887806bc81a23336753801021
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-04 20:14:23 +0100

    delimitamos la tabal T

---
+++ Ejercicio4/Codigo.py
@@ -0,0 +1,6 @@
+#"Tabla T"
+print("A continuacion, vamos a seleccionar los limites de la tabla T")
+print("Escriba el numero mas pequeño de la tabala que sea mayor que 0")
+a = int(input())
+print("Escriba el numero mas grande de la tabla")
+b = int(input())
